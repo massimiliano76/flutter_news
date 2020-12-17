2f6c76f92c99c9e188524fa7c5929e5daf5d47f7
Suhas Kadu <kadusuhas72@gmail.com>
2020-12-17 20:37:30 +0530

    feat:Splash screen changed and launcher icon changed

--- lib/views/home_page_2.dart
+++ lib/views/home_page_2.dart
@@ -17,14 +17,15 @@ class HomePage2 extends StatefulWidget {
   _HomePage2State createState() => _HomePage2State();
 }
 
-class _HomePage2State extends State<HomePage2> {
+class _HomePage2State extends State<HomePage2>
+    with SingleTickerProviderStateMixin {
   List<CategorieModel> categories = new List();
   List<ArticleModel> articles = new List();
   List<CategorieModel> sources = new List();
 
   bool _loading;
   var newsList;
-  TabController _tabController;
+  TabController tabController;
   int indexTab = 0;
 
   void getTrendingNews() async {
@@ -43,126 +44,132 @@ class _HomePage2State extends State<HomePage2> {
     categories = getCategories();
     getTrendingNews();
     sources = getSources();
-    //_tabController.dispose();
+
+    tabController = TabController(length: 3, vsync: this);
+    tabController.addListener(() {
+      if (tabController.index == 0) {
+        indexTab = 0;
+      } else {
+        indexTab = 1;
+      }
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 3,
-      child: Scaffold(
-        appBar: AppBar(
-          title: AppName(),
-          elevation: 0.0,
-          centerTitle: true,
-          bottom: TabBar(
-            unselectedLabelColor: Colors.grey,
-            isScrollable: false,
-            indicatorColor: Colors.blue[600],
-            indicatorWeight: 3,
-            indicatorSize: TabBarIndicatorSize.tab,
-            indicatorPadding: EdgeInsets.symmetric(horizontal: 12.0),
-            onTap: (index) {
-              setState(() {
-                indexTab = index;
-              });
-            },
-            tabs: [
-              Tab(
-                text: "TRENDING",
-              ),
-              Tab(
-                text: "CATEGORIES",
-              ),
-              Tab(
-                text: "SOURCES",
-              )
-            ],
-          ),
+    // ignore: missing_required_param
+    return Scaffold(
+      appBar: AppBar(
+        title: AppName(),
+        elevation: 0.0,
+        centerTitle: true,
+        bottom: TabBar(
+          unselectedLabelColor: Colors.grey,
+          isScrollable: false,
+          indicatorColor: Colors.blue[600],
+          indicatorWeight: 3,
+          indicatorSize: TabBarIndicatorSize.tab,
+          indicatorPadding: EdgeInsets.symmetric(horizontal: 12.0),
+          controller: tabController,
+          onTap: (index) {
+            setState(() {
+              indexTab = index;
+            });
+          },
+          tabs: [
+            Tab(
+              text: "TRENDING",
+            ),
+            Tab(
+              text: "CATEGORIES",
+            ),
+            Tab(
+              text: "SOURCES",
+            )
+          ],
         ),
-        body: DoubleBackToCloseApp(
-          snackBar: SnackBar(
-            content: Text("Tap again to exit "),
-          ),
-          child: ExtendedTabBarView(
-            controller: _tabController,
-            linkWithAncestor: false,
-            children: [
-              Scaffold(
-                body: Container(
-                  width: MediaQuery.of(context).size.width,
-                  margin: EdgeInsets.only(top: 3),
-                  child: ListView.builder(
-                      itemCount: articles.length,
-                      //shrinkWrap: true,
-                      physics: ClampingScrollPhysics(),
-                      itemBuilder: (context, index) {
-                        return NewsTile(
-                          imageUrl: articles[index].urlToImage ?? "",
-                          title: articles[index].title ?? "",
-                          description: articles[index].description ?? "",
-                          content: articles[index].content ?? "",
-                          postUrl: articles[index].url ?? "",
-                          author: articles[index].author ?? "",
-                          publishedAt: articles[index].publishedAt ?? "",
-                        );
-                      }),
-                ),
-                floatingActionButton: FloatingActionButton(
-                  //foregroundColor: Colors.black,
-                  child: Icon(
-                    Icons.brightness_6_sharp,
-                    color: Colors.black,
-                  ),
-                  onPressed: () {
-                    switchTheme();
-                  },
-                ),
-              ),
-              Container(
-                //margin: EdgeInsets.only(top: 10),
-                height: 200,
+      ),
+      body: DoubleBackToCloseApp(
+        snackBar: SnackBar(
+          content: Text("Tap again to exit "),
+        ),
+        child: ExtendedTabBarView(
+          controller: tabController,
+          children: [
+            Scaffold(
+              body: Container(
                 width: MediaQuery.of(context).size.width,
-                child: GridView.count(
-                  crossAxisCount: 2,
-                  childAspectRatio: 1.7,
-                  mainAxisSpacing: 6,
-                  crossAxisSpacing: 5,
-                  physics: ClampingScrollPhysics(),
-                  shrinkWrap: true,
-                  scrollDirection: Axis.vertical,
-                  padding: EdgeInsets.symmetric(horizontal: 12, vertical: 16.0),
-                  children: categories.map((category) {
-                    return CategorieTile2(
-                      imgUrl: category.imgUrl,
-                      title: category.categorieName,
-                    );
-                  }).toList(),
-                ),
+                margin: EdgeInsets.only(top: 3),
+                child: ListView.builder(
+                    itemCount: articles.length,
+                    //shrinkWrap: true,
+                    physics: ClampingScrollPhysics(),
+                    itemBuilder: (context, index) {
+                      return NewsTile(
+                        imageUrl: articles[index].urlToImage ?? "",
+                        title: articles[index].title ?? "",
+                        description: articles[index].description ?? "",
+                        content: articles[index].content ?? "",
+                        postUrl: articles[index].url ?? "",
+                        author: articles[index].author ?? "",
+                        publishedAt: articles[index].publishedAt ?? "",
+                      );
+                    }),
               ),
-              Container(
-                height: 200,
-                width: MediaQuery.of(context).size.width,
-                child: GridView.count(
-                  //itemCount: sources.length,
-                  crossAxisCount: 2,
-                  physics: ClampingScrollPhysics(),
-                  childAspectRatio: 1.6,
-                  shrinkWrap: true,
-                  mainAxisSpacing: 6,
-                  crossAxisSpacing: 7,
-                  scrollDirection: Axis.vertical,
-                  padding: EdgeInsets.symmetric(horizontal: 12, vertical: 16.0),
-                  children: sources.map((source) {
-                    return CategorieTile3(
-                      imgUrl: source.imgUrl,
-                      title: source.categorieName,
-                    );
-                  }).toList(),
+              floatingActionButton: FloatingActionButton(
+                backgroundColor: Colors.white,                
+                child: Icon(
+                  Icons.brightness_6_sharp,
+                  color: Colors.black,
                 ),
+                onPressed: () {
+                  switchTheme();
+                },
               ),
-            ],
-          ),
+            ),
+            Container(
+              //margin: EdgeInsets.only(top: 10),
+              height: 200,
+              width: MediaQuery.of(context).size.width,
+              child: GridView.count(
+                crossAxisCount: 2,
+                childAspectRatio: 1.7,
+                mainAxisSpacing: 6,
+                crossAxisSpacing: 5,
+                physics: ClampingScrollPhysics(),
+                shrinkWrap: true,
+                scrollDirection: Axis.vertical,
+                padding: EdgeInsets.symmetric(horizontal: 12, vertical: 16.0),
+                children: categories.map((category) {
+                  return CategorieTile2(
+                    imgUrl: category.imgUrl,
+                    title: category.categorieName,
+                  );
+                }).toList(),
+              ),
+            ),
+            Container(
+              height: 200,
+              width: MediaQuery.of(context).size.width,
+              child: GridView.count(
+                //itemCount: sources.length,
+                crossAxisCount: 2,
+                physics: ClampingScrollPhysics(),
+                childAspectRatio: 1.6,
+                shrinkWrap: true,
+                mainAxisSpacing: 6,
+                crossAxisSpacing: 7,
+                scrollDirection: Axis.vertical,
+                padding: EdgeInsets.symmetric(horizontal: 12, vertical: 16.0),
+                children: sources.map((source) {
+                  return CategorieTile3(
+                    imgUrl: source.imgUrl,
+                    title: source.categorieName,
+                  );
+                }).toList(),
+              ),
+            ),
+          ],
         ),
       ),
     );
